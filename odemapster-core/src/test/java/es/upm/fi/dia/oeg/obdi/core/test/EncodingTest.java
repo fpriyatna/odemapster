2647488e7d9a511074b153730c66e19f62d11216
Freddy Priyatna <fpriyatna@delicias.dia.fi.upm.es>
2012-06-20 11:04:12 +0000

    projects mavenized

---
+++ odemapster-core/src/test/java/es/upm/fi/dia/oeg/obdi/core/test/EncodingTest.java
@@ -0,0 +1,183 @@
+package es.upm.fi.dia.oeg.obdi.core.test;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+
+import es.upm.fi.dia.oeg.obdi.core.Utility;
+
+public class EncodingTest extends TestCase {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		String originalString = "http://www.google.com/Hello World# abc";
+		String encodedString = URLEncoder.encode(originalString, "UTF-8");
+		encodedString = encodedString.replaceAll("%3A", ":");
+		encodedString = encodedString.replaceAll("%2F", "/");
+		encodedString = encodedString.replaceAll("%23", "#");
+		
+		System.out.println("encodedString = " + encodedString);
+		
+
+	}
+	
+	@Test
+	public void testEncode01() throws Exception {
+		String str = "[\nab,c";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode02() throws Exception {
+		String str = "_";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode03() throws Exception {
+		String str = "\\";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode04() throws Exception {
+		String str = "	?";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode05() throws Exception {
+		String str = ",";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode06() throws Exception {
+		String str = "'";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode07() throws Exception {
+		String str = "abc def";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode08() throws Exception {
+		String str = "\"";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode09() throws Exception {
+		String str = "http://edu.linkeddata.es/UPM/resource/Actividad/Manual    de la calidad del Laboratorio de Ensayos Qu�micos Industriales , ((LEQIM)), Rev.10";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode10() throws Exception {
+		String str = "http://edu.linkeddata.es/UPM/resource/LineaInvestigaci%C3%B3n/31656_Análisis del Sector de las TIC\\s";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode11() throws Exception {
+		String str = "http://edu.linkeddata.es/UPM/resource/OtroParticipante/ Gallardo	_Fernando";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode12() throws Exception {
+		String str = "http://www.google.com/españa spain#lang=en,es";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode13() throws Exception {
+		String str = "http://geo.linkeddata.es/HospitalesMadrid#Hospitál110051";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode14() throws Exception {
+		String str = "http://edu.linkeddata.es/UPM/resource/Fecha/31/12/2004";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}
+
+	@Test
+	public void testEncode15() throws Exception {
+		String str = "http://edu.linkeddata.es/UPM/resource/OtroParticipante/LABORATORIO \"SALVADOR VELAYOS\"_INSTITUTO DE MAGNETISMO APLICADO";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+	
+	@Test
+	public void testEncode16() throws Exception {
+		String str = "Say \\r \"Hello World\"";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+
+	@Test
+	public void testEncode17() throws Exception {
+		String str = "Say \\n \'Hello World\'";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+
+	@Test
+	public void testEncode18() throws Exception {
+		String str = "Soledad_____Hurtado";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+	
+	@Test
+	public void testEncode19() throws Exception {
+		String str = "A & D ARQUITECTURA Y DECORACI�N 2000";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+
+	@Test
+	public void testEncode20() throws Exception {
+		String str = " ";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("Espacio en blanco (blank space) = " + strEncoded);
+	}	
+
+	@Test
+	public void testEncode21() throws Exception {
+		String str = "�� ��� ���� ���� ���� ���� Carlos   Mol";
+		String strEncoded = Utility.encodeURI(str);
+		System.out.println("strEncoded = " + strEncoded);
+	}	
+
+}
---
+++ odemapster-r2rml/src/main/java/es/upm/fi/dia/oeg/obdi/wrapper/r2rml/querytranslator/R2RMLAlphaGenerator.java
@@ -0,0 +1,177 @@
+package es.upm.fi.dia.oeg.obdi.wrapper.r2rml.querytranslator;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.log4j.Logger;
+
+import Zql.ZExp;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+import es.upm.fi.dia.oeg.obdi.core.model.AbstractConceptMapping;
+import es.upm.fi.dia.oeg.obdi.core.model.AbstractMappingDocument;
+import es.upm.fi.dia.oeg.obdi.core.model.AbstractPropertyMapping;
+import es.upm.fi.dia.oeg.obdi.core.querytranslator.AbstractAlphaGenerator;
+import es.upm.fi.dia.oeg.obdi.core.sql.SQLLogicalTable;
+import es.upm.fi.dia.oeg.obdi.core.sql.SQLQuery;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.R2RMLUtility;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.engine.R2RMLElementUnfoldVisitor;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.model.R2RMLJoinCondition;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.model.R2RMLLogicalTable;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.model.R2RMLPredicateObjectMap;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.model.R2RMLRefObjectMap;
+import es.upm.fi.dia.oeg.obdi.wrapper.r2rml.model.R2RMLTriplesMap;
+
+public class R2RMLAlphaGenerator extends AbstractAlphaGenerator {
+	private static Logger logger = Logger.getLogger(R2RMLAlphaGenerator.class);
+	private R2RMLElementUnfoldVisitor unfolder;
+	
+	public R2RMLAlphaGenerator(
+			Map<Node, Collection<AbstractConceptMapping>> mapNodeConceptMapping,
+			AbstractMappingDocument mappingDocument
+			, R2RMLElementUnfoldVisitor unfolder) {
+		super(mapNodeConceptMapping, mappingDocument);
+		this.unfolder = unfolder;
+	}
+
+	@Override
+	public Collection calculateAlpha(Triple tp) throws Exception {
+		Collection result = new Vector();
+		
+		//alpha subject
+		Node tpSubject = tp.getSubject();
+		Collection<AbstractConceptMapping> cms = super.mapNodeConceptMapping.get(tpSubject);
+		R2RMLTriplesMap cm = (R2RMLTriplesMap) cms.iterator().next();
+		SQLLogicalTable alphaSubject = this.calculateAlphaSubject(tpSubject, cm);
+		result.add(alphaSubject);
+		
+
+		//alpha predicate object
+		Node tpPredicate = tp.getPredicate();
+		Node tpObject = tp.getObject();
+		Collection<AbstractPropertyMapping> pms = cm.getPropertyMappings(tpPredicate.getURI());
+		if(pms != null && pms.size() > 0) {
+			R2RMLPredicateObjectMap pm = (R2RMLPredicateObjectMap) pms.iterator().next();
+			
+			R2RMLRefObjectMap refObjectMap = pm.getRefObjectMap();
+			if(refObjectMap != null) { 
+				SQLQuery alphaPredicateObject = 
+						this.calculateAlphaPredicateObject(pm, tp, cm);
+				result.add(alphaPredicateObject);
+			}
+		}
+
+		logger.debug("calculateAlpha = " + result);
+		return result;
+		
+
+	}
+
+	@Override
+	protected SQLQuery calculateAlphaPredicateObject(
+			AbstractPropertyMapping abstractPropertyMapping,
+			Triple triple, AbstractConceptMapping abstractConceptMapping) {
+		Node tpObject = triple.getObject();
+		R2RMLTriplesMap cm = (R2RMLTriplesMap) abstractConceptMapping;
+		//String tripleMapAlias = cm.getAlias();
+		String tripleMapAlias = cm.getLogicalTable().getAlias();
+		
+		SQLQuery joinQuery = null;
+		R2RMLPredicateObjectMap pm = (R2RMLPredicateObjectMap) abstractPropertyMapping;  
+		R2RMLRefObjectMap refObjectMap = pm.getRefObjectMap();
+		if(refObjectMap != null) { 
+			joinQuery = new SQLQuery();
+			joinQuery.setJoinType("INNER");
+			String joinQueryAlias = joinQuery.generateAlias();
+			joinQuery.setAlias(joinQueryAlias);
+			
+			//refObjectMap.setAlias(joinQueryAlias);
+			R2RMLQueryTranslator.mapTripleAlias.put(triple, joinQueryAlias);
+			
+			R2RMLLogicalTable parentLogicalTable = refObjectMap.getParentLogicalTable();
+			//SQLLogicalTable sqlParentLogicalTable = new R2RMLElementUnfoldVisitor().visit(parentLogicalTable);
+			SQLLogicalTable sqlParentLogicalTable = this.unfolder.visit(parentLogicalTable);
+			joinQuery.addLogicalTable(sqlParentLogicalTable);
+
+			Collection<R2RMLJoinCondition> joinConditions = refObjectMap.getJoinConditions();
+			ZExp onExpression = R2RMLUtility.generateJoinCondition(joinConditions, tripleMapAlias, joinQueryAlias);
+			if(onExpression != null) {
+				joinQuery.setOnExp(onExpression);
+			}
+		}
+
+		return joinQuery;
+	}
+
+//	@Override
+//	public AbstractConceptMapping calculateAlphaCM(Triple tp) throws Exception {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public AbstractConceptMapping calculateAlphaCMTB(Collection<Triple> triples)
+//			throws Exception {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	@Override
+	protected SQLLogicalTable calculateAlphaSubject(Node subject,
+			AbstractConceptMapping abstractConceptMapping) {
+		R2RMLTriplesMap cm = (R2RMLTriplesMap) abstractConceptMapping;
+		R2RMLLogicalTable r2rmlLogicalTable = cm.getLogicalTable();
+		//SQLLogicalTable sqlLogicalTable = new R2RMLElementUnfoldVisitor().visit(logicalTable);;
+		SQLLogicalTable sqlLogicalTable = this.unfolder.visit(r2rmlLogicalTable);;
+		String logicalTableAlias = sqlLogicalTable.generateAlias();
+		r2rmlLogicalTable.setAlias(logicalTableAlias);
+		sqlLogicalTable.setAlias(logicalTableAlias);
+//		cm.setAlias(logicalTableAlias);
+		return sqlLogicalTable;
+	}
+
+	public  Object calculateAlphaTB(Collection<Triple> triples)
+			throws Exception {
+		Collection result = new Vector();
+		
+		Triple firstTriple = triples.iterator().next();
+		Node tpSubject = firstTriple.getSubject();
+		Collection<AbstractConceptMapping> cms = super.mapNodeConceptMapping.get(tpSubject);
+		R2RMLTriplesMap cm = (R2RMLTriplesMap) cms.iterator().next();
+		SQLLogicalTable alphaSubject = this.calculateAlphaSubject(tpSubject, cm);
+		result.add(alphaSubject);
+		
+		
+		//mapping projection of corresponding predicates
+		for(Triple tp : triples) {
+			Node tpPredicate = tp.getPredicate();
+			String tpPredicateURI = tpPredicate.getURI();
+			boolean isRDFTypeStatement = RDF.type.getURI().equals(tpPredicateURI);
+			if(this.ignoreRDFTypeStatement && isRDFTypeStatement) {
+				//do nothing
+			} else {
+				Node tpObject = tp.getObject();
+				Collection<AbstractPropertyMapping> pms = cm.getPropertyMappings(tpPredicateURI);
+				if(pms != null) {
+					R2RMLPredicateObjectMap pm = (R2RMLPredicateObjectMap) pms.iterator().next();
+					logger.debug("pm = " + pm);
+					R2RMLRefObjectMap refObjectMap = pm.getRefObjectMap();
+					if(refObjectMap != null) { 
+						SQLQuery alphaPredicateObject = 
+								this.calculateAlphaPredicateObject(pm, tp, cm);
+						result.add(alphaPredicateObject);
+					}
+				}				
+			}
+		}
+
+		logger.debug("alphaTB = " + result);
+		return result;
+	}
+
+}
+ 
