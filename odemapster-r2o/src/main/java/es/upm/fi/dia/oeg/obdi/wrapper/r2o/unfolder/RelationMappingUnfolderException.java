3d3879f144de96a31bd0c1e8df04ba8f51f64122
Freddy Priyatna <freddy.priyatna@gmail.com>
2013-01-23 15:38:01 +0100

    remove some files

--- odemapster-r2o/src/main/java/es/upm/fi/dia/oeg/obdi/wrapper/r2o/unfolder/RelationMappingUnfolderException.java
+++
@@ -1,9 +0,0 @@
-package es.upm.fi.dia.oeg.obdi.wrapper.r2o.unfolder;
-
-public class RelationMappingUnfolderException extends Exception {
-
-	public RelationMappingUnfolderException(String errorMessage) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
