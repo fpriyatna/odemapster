61f0004e28abf54253987f17932f39ecfe7ac3b8
Freddy Priyatna <freddy.priyatna@gmail.com>
2013-01-24 15:08:49 +0100

    adding some examples

---
+++ odemapster-r2rml-querytranslator/src/test/java/es/upm/fi/dia/oeg/obdi/odemapster_r2rml_querytranslator/Example.java
@@ -0,0 +1,14 @@
+package es.upm.fi.dia.oeg.obdi.odemapster_r2rml_querytranslator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Example {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
