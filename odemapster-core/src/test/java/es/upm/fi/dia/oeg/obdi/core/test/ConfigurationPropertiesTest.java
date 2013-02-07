b6174d45cb1c0b76fa6cf01381c0859b8b2f05ed
Freddy Priyatna <freddy.priyatna@gmail.com>
2013-02-07 14:49:45 +0100

    cleaning odemapster-core project

--- odemapster-core/src/test/java/es/upm/fi/dia/oeg/obdi/core/test/ConfigurationPropertiesTest.java
+++ odemapster-core/src/test/java/es/upm/fi/dia/oeg/obdi/core/test/ConfigurationPropertiesTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import es.upm.fi.dia.oeg.obdi.core.engine.ConfigurationProperties;
+import es.upm.fi.dia.oeg.obdi.core.ConfigurationProperties;
 
 public class ConfigurationPropertiesTest {
 
--- odemapster-core/src/test/java/es/upm/fi/dia/oeg/obdi/core/test/GeneralTest.java
+++ odemapster-core/src/test/java/es/upm/fi/dia/oeg/obdi/core/test/GeneralTest.java
@@ -14,7 +14,7 @@ import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 import es.upm.fi.dia.oeg.obdi.core.ODEMapsterUtility;
-import es.upm.fi.dia.oeg.obdi.core.engine.QueryEvaluator;
+import es.upm.fi.dia.oeg.obdi.core.engine.RDBQueryEvaluator;
 
 public class GeneralTest {
 
