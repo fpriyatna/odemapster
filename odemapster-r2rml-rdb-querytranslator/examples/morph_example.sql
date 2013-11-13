9dde62fbe9c6c1c75052b73e4c77498037166426
fpriyatna <freddy.priyatna@gmail.com>
2013-11-13 16:19:49 +0100

    extending example tables

--- odemapster-r2rml-rdb-querytranslator/examples/morph_example.sql
+++ odemapster-r2rml-rdb-querytranslator/examples/morph_example.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 01, 2013 at 05:26 
--- Server version: 5.5.31
--- PHP Version: 5.4.16
+-- Generation Time: Nov 13, 2013 at 04:18 
+-- Server version: 5.6.12
+-- PHP Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,6 +31,8 @@ USE `morph_example`;
 CREATE TABLE IF NOT EXISTS `Sport` (
   `id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(50) DEFAULT NULL,
+  `code` char(8) DEFAULT NULL,
+  `type` char(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,8 +40,10 @@ CREATE TABLE IF NOT EXISTS `Sport` (
 -- Dumping data for table `Sport`
 --
 
-INSERT INTO `Sport` (`id`, `name`) VALUES
-(100, 'Tennis');
+INSERT INTO `Sport` (`id`, `name`, `code`, `type`) VALUES
+(100, 'Tennis', 'TNS', 'BOTH'),
+(200, 'Chess', 'CHS', 'INDOOR'),
+(300, 'Soccer', 'SCR', 'OUTDOOR');
 
 -- --------------------------------------------------------
 
@@ -65,8 +69,8 @@ CREATE TABLE IF NOT EXISTS `Student` (
 
 INSERT INTO `Student` (`id`, `name`, `sport`, `status`, `webpage`, `phone`, `email`) VALUES
 ('B1', 'Paul', 100, 'active', NULL, '777-3426', NULL),
-('B2', 'John', NULL, 'active', NULL, NULL, 'john@acd.edu'),
-('B3', 'George', NULL, 'active', 'www.george.edu', NULL, NULL),
+('B2', 'John', 200, 'active', NULL, NULL, 'john@acd.edu'),
+('B3', 'George', 300, 'active', 'www.george.edu', NULL, NULL),
 ('B4', 'Ringo', NULL, 'active', 'www.starr.edu', '888-4537', 'ringo@acd.edu');
 
 --
@@ -77,7 +81,7 @@ INSERT INTO `Student` (`id`, `name`, `sport`, `status`, `webpage`, `phone`, `ema
 -- Constraints for table `Student`
 --
 ALTER TABLE `Student`
-  ADD CONSTRAINT `Student_ibfk_1` FOREIGN KEY (`Sport`) REFERENCES `Sport` (`ID`);
+  ADD CONSTRAINT `Student_ibfk_1` FOREIGN KEY (`sport`) REFERENCES `Sport` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
